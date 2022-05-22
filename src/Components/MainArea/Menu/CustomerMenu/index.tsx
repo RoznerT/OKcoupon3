169786cb2256e665087c26f73a95aa2d83a343d1
Tal Rozner <92513165+RoznerT@users.noreply.github.com>
2022-05-22 12:08:48 +0300

    12:08

--- src/Components/MainArea/Menu/CustomerMenu/index.tsx
+++ src/Components/MainArea/Menu/CustomerMenu/index.tsx
@@ -1,5 +1,8 @@
+import { Button } from "@mui/material";
+
 function CustomerMenu() {
-    return ( null );
+
+    return ( <Button href="/home"> watch all coupons</Button> );
 }
 // each button redirect 
 // save the state of the tabs and add optional if the tab is equal to "getAllCoupons" 
